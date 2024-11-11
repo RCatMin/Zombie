1b0cd9185ecebded6298d0f65c0cc165911dc814
RCatMin <razzcat_min@naver.com>
2024-11-11 16:08:22 +0900

    Add getter & setter

--- Zombie/src/zombie/Unit.java
+++ Zombie/src/zombie/Unit.java
@@ -8,6 +8,33 @@ abstract public class Unit {
 	private int health;
 	private int position;
 	
-	int max;
+	int max; // 최대값
 
+	public Unit (int position, int health, int max) {
+		this.health = health;
+		this.position = position;
+		this.max = max;
+		
+		ran = new Random();
+	}
+	
+	public int getPosition () {
+		return position;
+	}
+	
+	public void setPosition (int position) {
+		this.position = position;
+	}
+	
+	public int getHealth () {
+		return health;
+	}
+	
+	public void setHealth (int health) {
+		this.health = health;
+	}
+	
+	public int getMax () {
+		return max;
+	}
 }
