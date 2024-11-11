0a6e1e251c4bddc3e6c701c0126b0bfdf4dbd307
RCatMin <razzcat_min@naver.com>
2024-11-11 16:56:52 +0900

    Create Zombie Class & Implement Zombie Attack Method

--- Zombie/src/zombie/Zombie.java
+++ Zombie/src/zombie/Zombie.java
@@ -1,5 +1,27 @@
 package zombie;
 
-public class Zombie {
+public class Zombie extends Unit{
+
+	int power;
+	
+	public Zombie(int position, int health, int max) {
+		super(position, health, max);
+	}
+	
+	public void attack (Unit Human) {
+		power = ran.nextInt(max);
+		
+		Human.setHealth(Human.getHealth() - power);
+		if (Human.getHealth() <= 0) {
+			Human.setHealth(0);
+		}
+		
+		if (power == 0) {
+			System.out.println ("좀비가 공격했으나 방어에 성공했습니다.");
+		} else {
+			String message = String.format("좀비가 %d의 피해를 입혔습니다. 남은 체력 : %d", power, Human.getHealth());
+			System.out.println (message);
+		}
+	}
 
 }
