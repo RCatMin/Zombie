84474b44e1c470bbe8c8baad221f8db3f131fc98
RCatMin <razzcat_min@naver.com>
2024-11-11 18:29:18 +0900

    Implement Boss Attack Method

--- Zombie/src/zombie/Boss.java
+++ Zombie/src/zombie/Boss.java
@@ -21,6 +21,24 @@ public class Boss extends Zombie {
 		this.megicPoint = megicPoint;
 	}
 	
+	public void attack (Unit Human) {
+		
+		int normalAttack = ran.nextInt(max);
+		
+		Human.setHealth(Human.getHealth() - power);
+		Human.setMegicPoint(Human.getMegicPoint() + power);
+		
+		if (normalAttack > 20) {
+			String message = String.format("Critical! 플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", normalAttack, Human.getHealth());
+			System.out.println (message);
+		} else {
+			String message = String.format("플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", normalAttack, Human.getHealth());
+			System.out.println (message);
+		}
+		
+		
+	}
+	
 	
 	
 
