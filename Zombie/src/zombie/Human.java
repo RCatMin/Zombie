642e11228c68e8b14fbb7a5adebc86f25fab1fb3
RCatMin <razzcat_min@naver.com>
2024-11-11 17:45:29 +0900

    Update Human Ammo Mechanism & Implement Attack Method

--- Zombie/src/zombie/Human.java
+++ Zombie/src/zombie/Human.java
@@ -12,8 +12,29 @@ public class Human extends Unit{
 		this.ammo = ammo;
 	}
 	
-	public void attack (Unit zombie) {
-		
+	public void attack (Unit enemy) {
+		if (enemy instanceof Boss) {
+			Boss boss = (Boss) enemy;
+			
+			ammo = 5;
+			power = ran.nextInt(max) + 3;
+			
+			if (ammo > 0) {				
+				boss.setHealth(boss.getHealth() - power);
+				boss.setMegicPoint(boss.getMegicPoint() + power / 2);
+			} else {
+				System.out.println ("재장전이 필요합니다.");
+			}
+			
+			if (boss.getHealth() <= 0) {
+				boss.setHealth(0);
+			}
+			
+			if (power >= 20) {
+				String message = String.format("Critical! 보스 좀비에게 치명상을 입혔습니다. \n 보스 체력 : %d", boss.getHealth());
+				System.out.println (message);
+			}
+		}
 	}
 
 }
