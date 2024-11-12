a089b4ddfe6b60550b03747d8b71253b0a9ce0c5
RCatMin <razzcat_min@naver.com>
2024-11-12 17:57:40 +0900

    Receive unit method and remove unused normalZombie integer.

--- Zombie/src/units/Boss.java
+++ Zombie/src/units/Boss.java
@@ -10,19 +10,17 @@ public class Boss extends Zombie {
 		super(position, health, max);
 	}
 
-	public void attack(Unit Human) {
+	public void attack(Unit unit) {
 
-		int normalAttack = ran.nextInt(max);
+		unit.setHealth(unit.getHealth() - power);
+		unit.setMegicPoint(unit.getMegicPoint() + power);
 
-		Human.setHealth(Human.getHealth() - power);
-		Human.setMegicPoint(Human.getMegicPoint() + power);
-
-		if (normalAttack > 20) {
-			String message = String.format("Critical! 플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", normalAttack,
-					Human.getHealth());
+		if (power > 20) {
+			String message = String.format("Critical! 플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", power,
+					unit.getHealth());
 			System.out.println(message);
 		} else {
-			String message = String.format("플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", normalAttack, Human.getHealth());
+			String message = String.format("플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", power, unit.getHealth());
 			System.out.println(message);
 		}
 	}
