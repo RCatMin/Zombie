1fbde6cd47ed79797307cfcb68a8327d6c0aca11
RCatMin <razzcat_min@naver.com>
2024-11-12 12:44:20 +0900

    Implement Player Megic Point System

--- Zombie/src/zombie/Boss.java
+++ Zombie/src/zombie/Boss.java
@@ -5,43 +5,26 @@ import java.util.Random;
 public class Boss extends Zombie {
 
 	Random ran = new Random();
-	
-	private int megicPoint;
-	
-	public int getMegicPoint() {
-		return megicPoint;
-	}
-	
-	public void setMegicPoint(int value) {
-		megicPoint = value;
-	}
-	
-	public Boss(int position, int health, int max, int megicPoint) {
+
+	public Boss(int position, int health, int max) {
 		super(position, health, max);
-		this.megicPoint = megicPoint;
 	}
-	
-	public void attack (Unit Human) {
-		
+
+	public void attack(Unit Human) {
+
 		int normalAttack = ran.nextInt(max);
-		
+
 		Human.setHealth(Human.getHealth() - power);
 		Human.setMegicPoint(Human.getMegicPoint() + power);
-		
+
 		if (normalAttack > 20) {
-			String message = String.format("Critical! 플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", normalAttack, Human.getHealth());
-			System.out.println (message);
+			String message = String.format("Critical! 플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", normalAttack,
+					Human.getHealth());
+			System.out.println(message);
 		} else {
 			String message = String.format("플레이어가 %d의 데미지를 받았습니다. \n 남은 체력 : %d", normalAttack, Human.getHealth());
-			System.out.println (message);
+			System.out.println(message);
 		}
-		
-		
 	}
-	
-	
-	
-
-	
 
 }
